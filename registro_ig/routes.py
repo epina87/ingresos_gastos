a0530df2cbbbf0fce92e9615116a582004337c6c
Enric Pina <epina87@gmail.com>
2022-09-15 23:30:18 +0200

    html

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,5 +1,10 @@
+from flask import render_template
 from registro_ig import app
 
 @app.route("/")
 def index():
-    return "Servidor rulando"
+    return render_template("index.html", pageTitle="Lista")
+
+@app.route("/nuevo")
+def alta():
+    return render_template("insert.html", pageTitle="Alta")
